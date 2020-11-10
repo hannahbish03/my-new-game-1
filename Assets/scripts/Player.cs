bbaf088863aed0a198b8a38b40569934bbfc7e7c
hannahbish <510894@chrome.escg.ac.uk>
2020-11-10 10:01:30 +0000

    saving my game

---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private PlayerStats stats;
+
+    private PlayerComponents components;
+
+    private PlayerRefrences refrences;
+
+    private PlayerUtilities utilities;
+
+    private PlayerActions actions; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scripts/PlayerComponents.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerComponents 
+{
+
+    private Rigidbody2D rigidbody2D; 
+
+
+
+
+
+}
